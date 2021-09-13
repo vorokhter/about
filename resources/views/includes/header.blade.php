a2cfae073661dd8aed807a0ce078f445b92e4453
Artyom Vorokhta <vorokhter@gmail.com>
2021-09-13 22:46:03 +0300

    wip

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,7 +13,7 @@
 
 
 <div class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
-    <div class="container d-flex">
+    <div class="container d-flex" style="max-width: 960px;">
         <a class="navbar-brand" href="{{ url('/') }}">
             about...
         </a>
--- resources/views/includes/message-list.blade.php
+++ resources/views/includes/message-list.blade.php
@@ -1,22 +1,9 @@
 @foreach($messages as $message)
-<!-- <div class="d-flex w-100 p-2 ">
-    <div class="card">
-        <div class="card-body">
-            <span class="text-primary" style="font-size: 12.5px">{{\App\Models\User::getUserNameById($message->user_id)}}</span>
-            <div>{{$message->text}}</div>
-        </div>
-    </div>
-</div> -->
-
 <div class="container mt-2">
-    <div>
+    <div class="d-flex flex-nowrap" style="font-size: 12.5px">
         <div class="text-primary">{{\App\Models\User::getUserNameById($message->user_id)}}</div>
-        <div class="text-muted">{{$message->created_at}}</div>
+        <div class="text-muted" style="margin-left: 5px;">{{date("H:i d.m.y", strtotime($message->created_at))}}</div>
     </div>
-
     <div>{{$message->text}}</div>
-
 </div>
 @endforeach
-
-<!-- {{ $currentUserId != $message->user_id ? 'justify-content-start' : 'justify-content-end' }} -->
