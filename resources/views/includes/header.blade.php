78259f6f164ab7c783f4c3a1c178e6bd640591aa
Artyom Vorokhta <vorokhter@gmail.com>
2021-09-14 01:02:44 +0300

    wip

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
     <div class="container d-flex" style="max-width: 960px;">
-        <a class="navbar-brand text-primary" href="{{ url('/') }}">
+        <a class="navbar-brand text-primary text-decoration-line-through" href="{{ url('/') }}">
             Штука чтобы сообщения писать
         </a>
         @include('includes.account-dropdown-menu')
