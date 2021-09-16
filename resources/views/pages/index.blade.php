f9481cfd083bc425229def826f81fe1b337ea7d6
Artyom Vorokhta <vorokhter@gmail.com>
2021-09-16 18:23:30 +0300

    wip

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,9 +9,9 @@
 @include('includes.header')
 
 <div class="container" style="max-width: 960px;">
-    <div class="row mt-3">
-        <div id="thread-list" class="col-md-2 list-group-flush rounded-1 overflow-auto" style="max-height: 50vh;"></div>
-        <div id="thread" class="col-md-8 p-2 border rounded-1 bg-white" style="display:none">
+    <div class="row">
+        <div id="thread-list" class="col-md-2 list-group rounded-1 overflow-auto mt-3" style="max-height: 50vh;"></div>
+        <div id="thread" class="col-md-8 p-2 border rounded-1 bg-white mt-3" style="display:none">
             <div id="message-list" class="overflow-auto mb-2" style="height: 50vh;"></div>
             <input id="input-message" class="form-control" type="text" name="message" placeholder="Введите сообщение">
         </div>
