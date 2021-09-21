1fa87590a519c20bc025339b132882e885e709a6
Artyom Vorokhta <vorokhter@gmail.com>
2021-09-21 23:09:26 +0300

    wip

--- public/static/js/pusher.js
+++ public/static/js/pusher.js
@@ -4,4 +4,9 @@ $(document).ready(function () {
     window.pusher = new Pusher("c3383bdc260bce376501", {
         cluster: "eu",
     });
+
+    var channel = pusher.subscribe("thread-channel");
+    channel.bind("new-message", function (data) {
+        console.log(JSON.stringify(data));
+    });
 });
