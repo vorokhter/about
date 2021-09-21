2b41744c84e9348938f9d801ffe2d4e99cb45ee3
Artyom Vorokhta <vorokhter@gmail.com>
2021-09-21 23:26:57 +0300

    debug

--- public/static/js/pusher.js
+++ public/static/js/pusher.js
@@ -1,12 +1,7 @@
 $(document).ready(function () {
-    Pusher.logToConsole = true;
+    Pusher.logToConsole = false;
 
     window.pusher = new Pusher("c3383bdc260bce376501", {
         cluster: "eu",
-    });
-
-    var channel = pusher.subscribe("thread-channel");
-    channel.bind("new-message", function (data) {
-        console.log(JSON.stringify(data));
     });
 });
