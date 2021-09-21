fc30b50fff5b7aa28efcd70817d54f6305a41b44
Artyom Vorokhta <vorokhter@gmail.com>
2021-09-21 23:13:24 +0300

    debug

--- app/Events/ThreadEvent.php
+++ app/Events/ThreadEvent.php
@@ -5,9 +5,9 @@ namespace App\Events;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class ThreadEvent implements ShouldBroadcast
+class ThreadEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
