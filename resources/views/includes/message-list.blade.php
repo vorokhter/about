2f5fd6194db1cf41a017a14dfe5debc1f14c5db7
Artyom Vorokhta <vorokhter@gmail.com>
2021-10-06 18:51:15 +0300

    debug

--- resources/views/includes/message-list.blade.php
+++ resources/views/includes/message-list.blade.php
@@ -9,7 +9,7 @@ $next_time = strtotime($message->created_at);
 $difference = ceil(($next_time - $last_time) / 60);
 ?>
 <div class="container mt-2 w-100 d-flex flex-nowrap">
-    <div class="d-flex justify-content-start" style="width: 52px;">
+    <div class="d-flex justify-content-start" style="min-width: 52px;">
         @if($message->user_id != $last_id or $difference > 5)
         <img id="header-avatar" src="{{\App\Models\User::getUserAvatarById($message->user_id)}}" alt="аватар пользователя" style="width: 36px; height: 36px; border-radius: 50%;">
         @endif
@@ -21,7 +21,7 @@ $difference = ceil(($next_time - $last_time) / 60);
             <div class="text-muted" style="margin-left: 5px;">{{date("H:i d.m.y", strtotime($message->created_at))}}</div>
         </div>
         @endif
-        <div>{{$message->text}}</div>
+        <div class="text-break">{{$message->text}}</div>
     </div>
 </div>
 <?php
