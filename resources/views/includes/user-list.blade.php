f7bffd7acc68eaabdbae73d617960ecc99b6d851
Artyom Vorokhta <vorokhter@gmail.com>
2021-10-26 17:00:49 +0300

    debug

--- resources/views/includes/user-list.blade.php
+++ resources/views/includes/user-list.blade.php
@@ -1,7 +1,10 @@
 @foreach($users as $user)
 <div class="container list-group-item user-name w-100 d-flex">
-    <img id="header-avatar" src="{{ $user->avatar }}" alt="аватар пользователя" style="width: 80px; height: 80px; border-radius: 50%; margin-right: 16px;">
-    <div class="w-100">
+    <div>
+        <img id="header-avatar" src="{{ $user->avatar }}" alt="аватар пользователя" style="width: 80px; height: 80px; border-radius: 50%;">
+    </div>
+
+    <div class="w-100" style="margin-left: 16px;">
         <div>{{$user->name}}</div>
         <div class="text-muted open-thread" style="cursor: pointer; font-size: 12.5px;" data-user-id="{{$user->id}}">Написать сообщение</div>
     </div>
